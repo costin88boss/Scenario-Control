0e2d2f9dec0b06169127bad099e741233f351293
BuildTools <unconfigured@null.spigotmc.org>
2021-01-26 18:36:29 +0200

    Add project files.

---
+++ ScenarioControl-EXILED2/Commands/Decontamination.cs
@@ -0,0 +1,57 @@
+﻿using CommandSystem;
+using Exiled.Events.EventArgs;
+using LightContainmentZoneDecontamination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScenarioControl_EXILED2.Commands
+{
+
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    class Decontamination : ICommand
+    {
+        static bool WillDecont;
+        public string Command { get; set; } = "Decont";
+
+        public string[] Aliases { get; set; } = { };
+
+        public string Description { get; set; } = "Disable or enable decontamination by doing 0 or 1 for disabled or enabled.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (arguments.At(0) != "")
+            {
+                if (arguments.At(0) == "0")
+                {
+                    WillDecont = true;
+                    response = "Decontamination is now disabled!";
+                    return true;
+                }
+                else if (arguments.At(0) == "1")
+                {
+                    WillDecont = false;
+                    response = "Decontamination is now enabled!";
+                    return true;
+                }
+                else
+                {
+                    response = "invalid argument. please input 0 for disabled or 1 for enabled.";
+                    return false;
+                }
+            }
+            else
+            {
+                response = "please input 0 for disabled or 1 for enabled.";
+                return false;
+            }
+        }
+
+        internal static void OnDecont(DecontaminatingEventArgs obj)
+        {
+            obj.IsAllowed = WillDecont;
+        }
+    }
+}
---
+++ ScenarioControl-EXILED2/ScenarioControl.cs
@@ -0,0 +1,47 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Map = Exiled.Events.Handlers.Map;
+
+namespace ScenarioControl_EXILED2
+{
+    public class ScenarioControl : Plugin<Config>
+    {
+        public Harmony Harmony { get; set; }
+        private int _PatchesThing;
+
+        public override void OnDisabled()
+        {
+            Harmony.UnpatchAll();
+        }
+        public override void OnEnabled()
+        {
+            try
+            {
+                Harmony = new Harmony($"ScenarioControl{++_PatchesThing}");
+
+                var LastDebugThing = Harmony.DEBUG;
+                Harmony.DEBUG = true;
+
+                Harmony.PatchAll();
+            }
+            catch(Exception e)
+            {
+                Log.Error("An error has occured: " + e);
+            }
+        }
+        public void Register()
+        {
+            Map.Decontaminating += ScenarioControl_EXILED2.Commands.Decontamination.OnDecont;
+        }
+        public void UnRegister()
+        {
+
+        }
+    }
+}
